a0918437e1c02a40e703338515dd28550efc1055
ahmedsamy <e-a.sami@lean.sa>
2022-09-26 11:02:17 +0200

    User registration and user profiles  - Extending the user model

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from account.forms import UserRegistrationForm
+from account.forms import UserRegistrationForm, UserEditForm, ProfileEditForm
+from account.models import Profile
 
 
 def register(request):
@@ -15,17 +16,38 @@ def register(request):
                 user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
-        return render(request,
-                      'account/register.html',
-                      {'user_form': user_form})
     else:
         user_form = UserRegistrationForm()
-        return render(request,
-                      'account/register.html',
-                      {'user_form': user_form})
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
 
 
 @login_required
