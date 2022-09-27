796f463fd388f084ebf227f4b1a9d807bc103e08
ahmedsamy <e-a.sami@lean.sa>
2022-09-27 15:28:08 +0200

    Building a bookmarklet with JavaScript

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -30,3 +30,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    import mimetypes
+
+    mimetypes.add_type('application/javascript', '.js', True)
+    mimetypes.add_type('text/css', '.css', True)
